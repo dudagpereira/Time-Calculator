ca44318c464700f76b16221da1665088ef3212c8
Maria Eduarda <maria.eduarda@pandhora.com>
2022-07-28 11:09:50 -0300

    first commit

---
+++ MaquinaDoTempo/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+    
+    
+//Pegar input de minutos e transformar em horas
+    //se for >= 60minutos, transformar em 1 horas
+    //se for < 60 minutos, adicionar a minutos
+
+
+//let horas = Array.empty
+//
+//let minutos = int(Console.ReadLine())
+//    if minutos >= 60
+//        horas.SetValue("one hour",0)
+//    else
+//        minutos
+//        
+ 
